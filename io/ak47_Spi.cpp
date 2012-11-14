6fdb1b1f22f3cc2153a47f151006a7e123855291
Francois Best <francoisbest@gmail.com>
2012-11-14 13:13:24 +0100

    Switched from virtually useless CC license to GPL.

--- io/ak47_Spi.cpp
+++ io/ak47_Spi.cpp
@@ -2,20 +2,17 @@
  *  \file       ak47_Spi.cpp
  *  \author     Francois Best
  *  \date       27/10/2012
- *  \license    CC-BY-SA Forty Seven Effects - 2012
+ *  \license    GPL v3.0 - Copyright Forty Seven Effects 2012
  *
- * THE WORK (AS DEFINED BELOW) IS PROVIDED UNDER THE TERMS 
- * OF THIS CREATIVE COMMONS PUBLIC LICENSE ("CCPL" OR "LICENSE").
- * THE WORK IS PROTECTED BY COPYRIGHT AND/OR OTHER APPLICABLE LAW. 
- * ANY USE OF THE WORK OTHER THAN AS AUTHORIZED UNDER THIS LICENSE 
- * OR COPYRIGHT LAW IS PROHIBITED.
- * 
- * BY EXERCISING ANY RIGHTS TO THE WORK PROVIDED HERE, YOU ACCEPT 
- * AND AGREE TO BE BOUND BY THE TERMS OF THIS LICENSE. 
- * TO THE EXTENT THIS LICENSE MAY BE CONSIDERED TO BE A CONTRACT, 
- * THE LICENSOR GRANTS YOU THE RIGHTS CONTAINED HERE IN CONSIDERATION 
- * OF YOUR ACCEPTANCE OF SUCH TERMS AND CONDITIONS.
- * http://creativecommons.org/licenses/by-sa/3.0/
+ *	This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details: http://www.gnu.org/licenses
  */
 
 #include "io/ak47_Spi.h"
