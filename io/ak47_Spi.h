33ca9ea499255615637eac5219370f7c246deee9
Francois Best <francoisbest@gmail.com>
2013-02-08 22:31:07 +0100

    Starting work on SPI.

--- io/ak47_Spi.h
+++ io/ak47_Spi.h
@@ -19,6 +19,7 @@
 
 #include "ak47.h"
 #include "ak47_Types.h"
+#include "memory/ak47_RingBuffer.h"
 #include <avr/io.h>
 
 BEGIN_AK47_NAMESPACE
@@ -26,12 +27,41 @@ BEGIN_AK47_NAMESPACE
 class Spi
 {
 public:
-    static inline void openMaster();
-    static inline void openSlave();
-    static inline void close();
+     Spi();
+    ~Spi();
     
 public:
-    static inline byte write(byte inData);
+    AVR_TYPEDEF_FUNCTOR(void, Parser, byte);
+    
+public:
+    inline void openMaster(Parser* inParser = 0);
+    inline void openSlave(Parser* inParser);
+    inline void close();
+    
+public: // Master only
+    inline void write(byte inData);
+    inline void write(const byte* inData, byte inLenght);
+    inline void busyWrite(byte inData);
+    inline void busyWrite(const byte* inData, byte inLenght);
+    
+public: // Master & Slave
+    inline bool available() const;
+    inline byte read();
+    
+public: // Master & Slave
+    inline void handleByteReceived(byte inData);
+    inline void handleEndOfTransmission();
+    
+public:
+    inline void clearRxBuffer();
+    inline void clearTxBuffer();
+    
+private:
+    static const byte sRxBufferSize = 16;
+    static const byte sTxBufferSize = 16;
+    RingBuffer<sRxBufferSize> mRxBuffer;
+    RingBuffer<sTxBufferSize> mTxBuffer;
+    Parser* mParser;
 };
 
 END_AK47_NAMESPACE
