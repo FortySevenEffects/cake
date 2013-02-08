e403051b76262b1a8e83f580b6a21e0ff9680ddd
Francois Best <francoisbest@gmail.com>
2013-02-08 22:24:56 +0100

    Fixed types.

--- memory/ak47_Memory.hpp
+++ memory/ak47_Memory.hpp
@@ -17,8 +17,8 @@
 
 #pragma once
 
-extern ak47::uint16 __heap_start;
-extern void*        __brkval;
+extern uint16 __heap_start;
+extern void*  __brkval;
 
 BEGIN_AK47_NAMESPACE
 
@@ -33,7 +33,7 @@ uint16 getHeapSize()
     return (uint16) &dummy - (__brkval == 0 ? (uint16) &__heap_start : (uint16) __brkval);
 }
 
-ak47::uint16 getStackSize()
+uint16 getStackSize()
 {
     return RAMEND - SP;
 }
